490cc7f2c07bc54d1571557463c7642171b4896c
Bakr Al-Humaimidi <bakir_omer66@yahoo.com>
2019-04-07 18:46:20 -0700

    New version of App

---
+++ config/auth/firebase_info_real.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhmSuCV7zn69EaAJKMMRgkFlern_McwK8",
+  authDomain: "mobile-fitness-planner.firebaseapp.com",
+  databaseURL: "https://mobile-fitness-planner.firebaseio.com",
+  projectId: "mobile-fitness-planner",
+  storageBucket: "mobile-fitness-planner.appspot.com",
+  messagingSenderId: "928462755945"
+};
